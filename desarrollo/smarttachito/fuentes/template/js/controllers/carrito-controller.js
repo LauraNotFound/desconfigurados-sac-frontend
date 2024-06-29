fe5423956d83483acf366b35a5763b505f94961f
anycodef <anycodefast1@gmail.com>
2024-06-29 14:13:47 -0500

    Reparar y documentar el funcionamiento del controlador

--- desarrollo/smarttachito/fuentes/template/js/controllers/carrito-controller.js
+++ desarrollo/smarttachito/fuentes/template/js/controllers/carrito-controller.js
@@ -12,12 +12,15 @@ listCarrito = [
     {
         id: "5",
         amount: 1
+    },
+    {
+        id: "13"
+        amount: 2
     }
 ]
 */
 
-
-
+// obtener la lista que maneja el carrito del localstorage
 function get_lista_producto_carrito() {
     // obtener lista de ids de productos en el carrito
     let listCarrito = localStorage.getItem('cart');
@@ -32,6 +35,41 @@ function get_lista_producto_carrito() {
     return listCarrito;
 }
 
+function set_lista_producto_carrito(lista_carrito) {
+    localStorage.setItem('cart', JSON.stringify(lista_carrito))
+}
+
+// introducir nuevos valores al localstorage
+function refresh_lista_producto_carrito(id, amount) {
+    // obtener lista del localstorage
+    let lista_producto_carrito = get_lista_producto_carrito();
+
+    // introducir nuevo valor o adicionar el existente
+    // productStorage es igual a {id: "algun-id", amout: "cantidad-producto"}
+    let index = 0;
+    while (index < lista_producto_carrito.length && id != lista_producto_carrito[index].id) {
+        index++;
+    }
+
+    // si se cumple la igualdad quiere decir que 
+    // recorrimos toda la lista sin hallar el valor
+    // osea no existe ese producto en el carrito
+    if (index == lista_producto_carrito.length) {
+        lista_producto_carrito.push({
+            "id": id,
+            "amount": amount
+        });
+    } else { // caso contrario solo aumentamos el valor 
+        lista_producto_carrito[index].amount += amount;
+    }
+
+    // añadir los cambios al localstorage
+    set_lista_producto_carrito(lista_producto_carrito);
+}
+
+// Obtiene la lista completa del carrito
+// calcula la cantidad total de productos
+// modifica el valor en el html
 function actualizarCantidadCarrito() {
     const contenedorCantidadCarrito = document.getElementById('contenedor-cantidad-carrito');
     let cantidadProductosCarrito = 0;
@@ -41,47 +79,21 @@ function actualizarCantidadCarrito() {
     contenedorCantidadCarrito.innerHTML = `(${cantidadProductosCarrito})`;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
 
+document.addEventListener('DOMContentLoaded', () => {
     // actualizar el contador del carrito al iniciar
     actualizarCantidadCarrito();
     
-    // establecer el comportamiento del boton add to cart  
-    document.querySelectorAll('.button-add-to-cart').forEach(buttonAddToCart => {
-        buttonAddToCart.addEventListener('click', () => {
+    // establecer el comportamiento añadir al carrito de los producto mostrados
+    document.querySelectorAll('.contenedor-producto').forEach(productContainer => {
+        console.log(productContainer);
 
-            // obtener lista de diccionarios keys[id, amount] de productos en el carrito
-            let listCarrito = get_lista_producto_carrito();
-    
-            let cantidadProducto = this.closest('.cantidad-producto').getAttribute('value');
-            const idProducto = this.closest('.contenedor-producto').getAttribute('data-producto-id');
-
-            // si no se tiene el dato de la cantidad ingresada entonces se asume el predeterminado que es uno
-            if(!cantidadProducto) {
-                cantidadProducto = 1;
-            }
+        // encontrar el botón add to cart asociarle el evento click
+        productContainer.querySelector('.button-add-to-cart').addEventListener('click', () => {
+            const id_product = productContainer.getAttribute('data-producto-id');
+            let amount_product = productContainer.querySelector('#cantidad-producto') ? parseInt(productContainer.querySelector('#cantidad-producto').value) : 1;
             
-            // verificamos si ya existe el producto en el carrito para adicionar la cantidad al producto en el carrito existente
-            let estaEnCarro = false;
-            let index = 0;
-            while(!estaEnCarro && index < listCarrito.length) {
-                estaEnCarro = listCarrito[index].id === idProducto; 
-                index++;
-            }
-
-            // adicionamos o cremos un producto en el carrito según sea el caso
-            if ( estaEnCarro ) {
-                listCarrito[index - 1].amount += cantidadProducto;
-            } else {
-                listCarrito.push({
-                    id: idProducto,
-                    amount: cantidadProducto
-                });    
-            }
-
-            // hacer persistente la lista
-            localStorage.setItem('cart', JSON.stringify(listCarrito));
-
+            refresh_lista_producto_carrito(id_product, amount_product);
         });
     });
 });
@@ -93,3 +105,9 @@ window.addEventListener('storageChange', (event) => {
     }
 });
 
+// documentacion
+// para usar el controlador:
+// 1) colocar la clase "contenedor-producto" en el div o contenedor padre de cada producto (incluyendo el modal)
+//    y ademas colocar el atributo data-producto-id con el id correspondiente a ese producto.
+// 2) en el input de cantidad de producto del modal colocar el id cantidad-producto. Ejemplo (<input class="form-control border-0 shadow-0 p-0" id="cantidad-producto" type="text" value="1">)
+// 3) y en todos los botones add-to-cart colocar la clase button-add-to-cart
