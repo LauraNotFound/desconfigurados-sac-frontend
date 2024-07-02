3303b8b39c260d5cd36e79a4ea3cf7ccdfcbefb2
anycodef <anycodefast1@gmail.com>
2024-07-02 01:51:05 -0500

    Asociar función para guardar producto en el carrito con el onclick del botón Add to cart

--- desarrollo/smarttachito/fuentes/template/js/productos.js
+++ desarrollo/smarttachito/fuentes/template/js/productos.js
@@ -1,3 +1,5 @@
+import { guardar_producto_en_carrito } from "./controllers/carrito";
+
 const productContainer = document.getElementById('product-container');
 const categoryContainer = document.getElementById('category-container');
 const priceSlider = document.getElementById('price-slider');
@@ -93,7 +95,7 @@ const createProductCard = (product) => {
                 <div class="product-overlay">
                     <ul class="mb-0 list-inline">
                         <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#!"><i class="far fa-heart"></i></a></li>
-                        <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark" href="cart.html">Add to cart</a></li>
+                        <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark" href="cart.html" onclick='guardar_producto_en_carrito(${product.url}, ${product}, 1)'>Add to cart</a></li>
                         <li class="list-inline-item mr-0"><a class="btn btn-sm btn-outline-dark" href="#productView" data-bs-toggle="modal"><i class="fas fa-expand"></i></a></li>
                     </ul>
                 </div>
